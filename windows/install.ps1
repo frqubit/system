ed7a1cebc6622284ee9b0f358cc3320ffb4ee9ff
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-09-21 15:08:46 -0400

    added more choco packages

--- windows/install.ps1
+++ windows/install.ps1
@@ -19,7 +19,11 @@ try {
     if (Get-Command choco) {
         Write-Host "Choco is installed! Installing packages..."
 
-        choco install git googlechrome vscode -y
+        choco install `
+            git googlechrome vscode discord `
+            krita kdenlive obs-studio audacity `
+            blender spotify `
+            -y
     }
 } Catch {
     powershell.exe "$REPO_DIR\windows\scripts\chocolatey.ps1"
