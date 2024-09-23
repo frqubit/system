457660966f53656c5bab9dcc613537e38b9f6e43
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-09-23 15:43:45 -0400

    added update flag

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+arg0=$0
+
+help_msg() {
+    echo "$arg0 - set up a linux debian system"
+    echo " "
+    echo "$arg0 [options]"
+    echo " "
+    echo "options:"
+    echo "-h, --help             show brief help"
+    echo "--update               only update, do not install stuff"
+    exit 0
+}
+
 chmod +x linux/scripts/*.sh
 
 REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/linux
@@ -7,6 +20,23 @@ echo "REPO_DIR = $REPO_DIR"
 
 # Process extras
 EXTRAS_FILE=$(mktemp)
+
+# Process flags
+while test $# -gt 0; do
+    case $1 in
+        -h|--help)
+            help_msg
+            exit 0
+            ;;
+        --update)
+            shift
+            echo "update_only" >> $EXTRAS_FILE
+            ;;
+        -*)
+            break
+            ;;
+    esac
+done
 
 pgrep spotify &> /dev/null
 
@@ -25,39 +55,70 @@ echo ""
 cat $EXTRAS_FILE
 
 cat linux/scripts/usermap | while read line; do
-    shell_name=linux/scripts/$(echo $line | awk '{print $1;}')
-    user=$(echo $line | awk '{print $2;}')
+    shell_name=$(echo $line | awk '{print $1;}')
 
     if [[ "$shell_name" == "linux/scripts/#" ]]; then
         echo "skipping $user"
     else
-        export REPO_DIR
+        shell_name=linux/scripts/$shell_name
+        user=$(echo $line | awk '{print $2;}')
 
-        if [[ "$user" == "root" ]]; then
-            echo ""
-            echo "#########################"
-            echo "# (ROOT) $shell_name"
-            echo "#########################"
-            echo ""
+        specs=$(echo $line | cut -d" " -f3-)
+        specs=($specs)
 
-            sudo REPO_DIR=$REPO_DIR EXTRAS_FILE=$EXTRAS_FILE $shell_name
+        actually_run=1
 
-            if [ "$?" -ne "0" ]; then
-                echo "Failed script: [$shell_name]"
-                exit 1
+        for spec in "${specs[@]}"; do
+            if [[ $spec == '!'* ]]; then
+                spec="${spec:1}"
+
+                grep -Fxq $spec $EXTRAS_FILE
+                if [ "$?" -eq "0" ]; then
+                    echo "[$shell_name] Cannot run this: $spec exists"
+
+                    actually_run=0
+                    break
+                fi
+            else
+                grep -Fxq $spec $EXTRAS_FILE
+                if [ "$?" -ne "0" ]; then
+                    echo "[$shell_name] Cannot run this: $spec does not exist"
+
+                    actually_run=0
+                    break
+                fi
             fi
-        else
-            echo ""
-            echo "#########################"
-            echo "# $shell_name"
-            echo "#########################"
-            echo ""
+        done
 
-            REPO_DIR=$REPO_DIR EXTRAS_FILE=$EXTRAS_FILE $shell_name
+        if [ "$actually_run" -eq "1" ]; then
+            export REPO_DIR
 
-            if [ "$?" -ne "0" ]; then
-                echo "Failed script: [$shell_name]"
-                exit 1
+            if [[ "$user" == "root" ]]; then
+                echo ""
+                echo "#########################"
+                echo "# (ROOT) $shell_name"
+                echo "#########################"
+                echo ""
+
+                sudo REPO_DIR=$REPO_DIR EXTRAS_FILE=$EXTRAS_FILE $shell_name
+
+                if [ "$?" -ne "0" ]; then
+                    echo "Failed script: [$shell_name]"
+                    exit 1
+                fi
+            else
+                echo ""
+                echo "#########################"
+                echo "# $shell_name"
+                echo "#########################"
+                echo ""
+
+                REPO_DIR=$REPO_DIR EXTRAS_FILE=$EXTRAS_FILE $shell_name
+
+                if [ "$?" -ne "0" ]; then
+                    echo "Failed script: [$shell_name]"
+                    exit 1
+                fi
             fi
         fi
     fi
--- linux/scripts/discord.sh
+++ linux/scripts/discord.sh
@@ -2,10 +2,28 @@
 
 which discord >/dev/null
 
-if [ "$?" -eq "1" ]; then
-    curl "https://dl.discordapp.net/apps/linux/0.0.68/discord-0.0.69.deb" -o discord.deb
+LATEST_VERSION="0.0.69"
+
+install_discord() {
+    curl "https://dl.discordapp.net/apps/linux/$LATEST_VERSION/discord-$LATEST_VERSION.deb" -o discord.deb
     sudo dpkg -i discord.deb
     rm discord.deb
+}
+
+if [ "$?" -eq "1" ]; then
+    # Discord is not installed
+    install_discord
 else
     echo "[$0] discord is already installed"
+    echo "[$0] checking for updates..."
+
+    discord_version=$(dpkg -s discord | grep '^Version:' | awk '{print $2}')
+
+    if [[ ! "$discord_version" == "$LATEST_VERSION" ]]; then
+        echo "[$0] updating discord..."
+
+        install_discord
+    else
+        echo "[$0] discord is up to date!"
+    fi
 fi
