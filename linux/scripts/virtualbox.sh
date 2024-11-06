51bdc9a9446f70743c0fa15793b0275624e83871
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-11-06 09:15:26 -0500

    updated discord and virtualbox

--- linux/scripts/virtualbox.sh
+++ linux/scripts/virtualbox.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne "0" ]; then
-    echo "[$0] this script requires root"
-    exit 1
-fi
-
-which virtualbox >/dev/null
-
-if [ "$?" -eq "1" ]; then
-    curl "https://download.virtualbox.org/virtualbox/7.0.20/virtualbox-7.0_7.0.20-163906~Debian~bookworm_amd64.deb" -o virtualbox.deb
-    sudo dpkg -i virtualbox.deb
-    rm virtualbox.deb
+if [ ! -f /etc/apt/sources.list.d/virtualbox.list ]; then
+    # firefox key
+    wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list > /dev/null
 else
-    echo "[$0] virtualbox is already installed"
+    echo "[$0] virtualbox key is already imported"
 fi
+
+apt-get update
+apt-get install virtualbox-7.1 -y
